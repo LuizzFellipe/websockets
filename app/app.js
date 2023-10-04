8e833495b1047698cb7ab1850bfad9d35b3cb3a0
LuizzFellipe <luiz.fellipe@aluno.ifsp.edu.br>
2023-10-04 16:02:06 -0300

    Version 2.0.0, Instalação do Framework socket.io

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
-const socket = new WebSocket('ws://localhost:3000')
+const socket = io('ws://localhost:3000')
 
 function sendMessage(e) {
     e.preventDefault()
     const input = document.querySelector('input')
-    if(input.value) {
-        socket.send(input.value)
+    if (input.value) {
+        socket.emit('message', input.value)
         input.value = ""
     }
     input.focus()
@@ -16,7 +16,7 @@ document.querySelector('form')
 
 
 // Lista de Menssage
-socket.addEventListener("message", ({ data }) => {
+socket.addEventListener("message", (data) => {
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
